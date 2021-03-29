cb8985846e18d5e680f916836c2f3ea28a67177f
KMStr <kstrigalev@gmail.com>
2021-03-29 17:34:53 +0300

    2-е ДЗ (код с 3-го урока)

---
+++ hw3/app/src/main/java/ru/strigalev/myweatherforecast/viewmodel/AppState.kt
@@ -0,0 +1,9 @@
+package ru.strigalev.myweatherforecast.viewmodel
+
+import ru.strigalev.myweatherforecast.model.Weather
+
+sealed class AppState {
+    data class Success(val weatherData: List<Weather>) : AppState()
+    data class Error(val error: Throwable) : AppState()
+    object Loading : AppState()
+}
---
+++ hw3/app/src/main/java/ru/strigalev/myweatherforecast/viewmodel/MainViewModel.kt
@@ -0,0 +1,30 @@
+package ru.strigalev.myweatherforecast.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.strigalev.myweatherforecast.model.Repository
+import ru.strigalev.myweatherforecast.model.RepositoryImpl
+import java.lang.Thread.sleep
+
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()
+) :
+    ViewModel() {
+
+    fun getLiveData() = liveDataToObserve
+
+    fun getWeatherFromLocalSourceRus() = getDataFromLocalSource(isRussian = true)
+
+    fun getWeatherFromLocalSourceWorld() = getDataFromLocalSource(isRussian = false)
+
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource(isRussian = true)
+
+    private fun getDataFromLocalSource(isRussian: Boolean) {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(if (isRussian) repositoryImpl.getWeatherFromLocalStorageRus() else repositoryImpl.getWeatherFromLocalStorageWorld()))
+        }.start()
+    }
+}
